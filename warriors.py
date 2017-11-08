8de61523180bffb28ebd02c39a242f36c796e6ce
tburly <threefold.burly@gmail.com>
2017-11-08 23:34:45 +0100

    Initial commit

---
+++ warriors.py
@@ -0,0 +1,4 @@
+class Warrior:
+    """Fights, equips, drops. Has: name, health, was_hit, items, offense, defense, to_parry, to_block"""
+
+    def __init__(self, name, health=30, was_hit=False, items=[])
